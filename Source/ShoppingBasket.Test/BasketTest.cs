4b838a68889eba62bb5fb5575c0efca81e2e8103
Branimir Šajinović <bsajinovic@span.eu>
2020-02-04 15:29:38 +0100

    Small basket test refactor + comments

--- Source/ShoppingBasket.Test/BasketTest.cs
+++ Source/ShoppingBasket.Test/BasketTest.cs
@@ -7,17 +7,27 @@ namespace ShoppingBasket.Test
 {
     public class BasketTest
     {
+        /// <summary>
+        /// Instance of the basket to test.
+        /// </summary>
+        private readonly IBasket basket = new Basket();
+
+        /// <summary>
+        /// Data to test the total price of the items in the basket when no discounts are applied.
+        /// </summary>
+        /// <returns></returns>
         public static List<object[]> GetTestProductsWithTheirPricesSum() => new List<object[]>()
         {
+             // Empty basket, total should be zero
             new object[] { new Product[0], 0 },
+
+             // Three products, total should sum of their prices
             new object[] { new[] { new Product(ProductType.Bread, "Bread", 1), new Product(ProductType.Butter, "Butter", 0.8m), new Product(ProductType.Milk, "Milk", 1.15m) }, 2.95m }
         };
 
         [Fact]
         public void AddProductThrowsArgNullExceptionWhenAddingNullProducts()
         {
-            var basket = new Basket();
-
             Assert.Throws<ArgumentNullException>(() => basket.AddProduct(null));
         }
 
@@ -25,8 +35,6 @@ namespace ShoppingBasket.Test
         [MemberData(nameof(GetTestProductsWithTheirPricesSum))]
         public void TotalIsSumOfProductPricesWhenNoDiscounts(IEnumerable<Product> products, decimal total)
         {
-            var basket = new Basket();
-
             foreach (var product in products)
             {
                 basket.AddProduct(product);
